f939d4f3f5318eaf056c1cff585e1ddacec154fe
KoalaChang <nobelesse799@gmail.com>
2025-11-13 17:22:37 +0800

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by 張宇涵 on 2025/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State private var showingAlert = false
+
+  var body: some View {
+    Button("Show alert") {
+      showingAlert = true
+    }
+    .alert("Important message", isPresented: $showingAlert) {
+      Button("Delete", role: .destructive) {}
+      Button("Cancel", role: .cancel) {}
+    } message: {
+      Text("Please read this.")
+    }.alert("Important message", isPresented: $showingAlert) {
+      Button("Delete", role: .destructive) {}
+      Button("Cancel", role: .cancel) {}
+    } message: {
+      Text("Please read this.")
+    }.alert("Important message", isPresented: $showingAlert) {
+      Button("Delete", role: .destructive) {}
+      Button("Cancel", role: .cancel) {}
+    } message: {
+      Text("Please read this.")
+    }.alert("Important message", isPresented: $showingAlert) {
+      Button("Delete", role: .destructive) {}
+      Button("Cancel", role: .cancel) {}
+    } message: {
+      Text("Please read this.")
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by 張宇涵 on 2025/11/13.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
